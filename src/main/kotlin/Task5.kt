1259a81c9bbb6956d252d609018568871ca72246
danilshik <danilshik@yandex.ru>
2022-09-11 17:04:57 +0300

    Complete task 5

---
+++ src/main/kotlin/Task5.kt
@@ -0,0 +1,27 @@
+class Task5 {
+    fun longestPalindrome(s: String?): String {
+        if (s == null || s.isEmpty()) return ""
+        var start = 0
+        var end = 0
+        for (i in s.indices) {
+            val len1 = expandAroundCenter(s, i, i)
+            val len2 = expandAroundCenter(s, i, i + 1)
+            val len = Math.max(len1, len2)
+            if (len > end - start) {
+                start = i - (len - 1) / 2
+                end = i + len / 2
+            }
+        }
+        return s.substring(start, end + 1)
+    }
+
+    private fun expandAroundCenter(s: String, left: Int, right: Int): Int {
+        var l = left
+        var r = right
+        while (l >= 0 && r < s.length && s[l] == s[r]) {
+            l--
+            r++
+        }
+        return r - l - 1
+    }
+}
